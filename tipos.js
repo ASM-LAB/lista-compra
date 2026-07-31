09160112dc0aea4040e67bd9cd725a1cd3083729
Alfredo <54837358+ASM-LAB@users.noreply.github.com>
2026-07-31 20:11:20 +0200

    Add files via upload

---
+++ tipos.js
@@ -0,0 +1,10 @@
+// tipos.js
+export const TIPOS_COMPRA = [
+    { id: 'carnes', label: 'Carnes' },
+    { id: 'pescado', label: 'Pescado' },
+    { id: 'drogueria', label: 'Droguería' },
+    { id: 'lacteos', label: 'Lácteos' },
+    { id: 'verduras', label: 'Verduras' },
+    { id: 'frutas', label: 'Frutas' },
+    { id: 'otros', label: 'Otros' }
+];
