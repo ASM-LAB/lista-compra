2732441b2c80b9d12c9d26d31ee0a082d00b145a
Alfredo <54837358+ASM-LAB@users.noreply.github.com>
2026-08-08 18:15:38 +0200

    Add files via upload

--- Creacion tablas.sql
+++ Creacion tablas.sql
@@ -1,9 +1,13 @@
 -- Crear la tabla de productos
-create table productos (
-  id bigint primary key,
+create table public.productos (
+  id bigint not null,
   nombre text not null,
-  seleccionado boolean default false
-);
+  seleccionado boolean null default false,
+  tipo character varying(50) null default 'otros'::character varying,
+  cantidad integer not null default 1,
+  orden integer not null default 0,
+  constraint productos_pkey primary key (id)
+) TABLESPACE pg_default;
 
 -- Habilitar el tiempo real para esta tabla
 alter publication supabase_realtime add table productos;
@@ -11,3 +15,5 @@ alter publication supabase_realtime add table productos;
 -- (Opcional) Desactivar la seguridad estricta para pruebas sencillas sin login
 alter table productos enable row level security;
 create policy "Acceso público total" on productos for all using (true) with check (true);
+
+
